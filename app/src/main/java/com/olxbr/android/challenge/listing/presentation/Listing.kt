5866c62def4907efaf372c1e0ed11204d3ace797
anaandreis <anapauladeandreis@gmail.com>
2023-06-24 23:22:17 -0300

    TextField/UI changes in Listing - created a variable to store query to prevent lag - sends query to filter function only when user clicks search button on Keyboard - Added Launched Effect to show all ads when user deletes current search - hides keyboard when search button in clicked - added message to user when search returns empty

--- app/src/main/java/com/olxbr/android/challenge/listing/presentation/Listing.kt
+++ app/src/main/java/com/olxbr/android/challenge/listing/presentation/Listing.kt
@@ -5,14 +5,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -21,22 +31,53 @@ fun Listing(
     state: ListingState.Success,
     onAction: (ListingAction) -> Unit = {}
 ) {
+
+    val focusManager = LocalFocusManager.current
+
     Column {
-        TextField(
-            value = state.query ?: "",
-            onValueChange = {
-                onAction(ListingAction.Filter(it))
-            },
+
+        val queryValue = remember { mutableStateOf(state.query ?: "") }
+
+        //trigger filter function and shows all ads when query is empty
+        LaunchedEffect(queryValue.value) {
+            if (queryValue.value.isBlank()) {
+                onAction(ListingAction.Filter(""))
+            }
+        }
+
+        OutlinedTextField(
+            value = queryValue.value,
+            onValueChange = { queryValue.value = it },
             leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
             label = { Text(text = "Pesquisar") },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp)
-        )
+                .padding(8.dp),
+            shape = RoundedCornerShape(10.dp),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions( onSearch = {
+                onAction(ListingAction.Filter(queryValue.value))
+                focusManager.clearFocus()
+            }),
 
+
+            )
         LazyColumn {
             items(state.ads) { ad ->
                 AdCard(ad)
+            }
+
+            if (state.ads.isEmpty()) {
+                item {
+                    Text(
+                        text = "Nenhum resultado encontrado.",
+                        modifier = Modifier.fillMaxWidth().padding(16.dp),
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
         }
     }
