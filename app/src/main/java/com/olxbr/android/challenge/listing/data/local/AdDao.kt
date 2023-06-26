2cb2fbce65d59f5285cecb2b7d2dd35ecc1fb7e4
anaandreis <anapauladeandreis@gmail.com>
2023-06-26 10:07:24 -0300

    Added AdDatabase and AdDao for storing offline cache

---
+++ app/src/main/java/com/olxbr/android/challenge/listing/data/local/AdDao.kt
@@ -0,0 +1,19 @@
+package com.olxbr.android.challenge.listing.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.olxbr.android.challenge.listing.model.Ad
+
+
+@Dao
+interface AdDao {
+
+    @Query("SELECT * FROM ad")
+    suspend fun getLocalAds(): List<Ad>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAds(ads: List<Ad>)
+
+}
---
+++ app/src/main/java/com/olxbr/android/challenge/listing/data/local/AdDatabase.kt
@@ -0,0 +1,32 @@
+package com.olxbr.android.challenge.listing.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.olxbr.android.challenge.listing.model.Ad
+
+
+@Database(entities = [Ad::class], version = 1)
+abstract class AdDatabase : RoomDatabase() {
+    abstract fun adDao(): AdDao
+
+    companion object {
+        private const val DATABASE_NAME = "ad-database"
+
+        @Volatile
+        private var INSTANCE: AdDatabase? = null
+
+        fun getInstance(context: Context): AdDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AdDatabase::class.java,
+                    DATABASE_NAME
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
